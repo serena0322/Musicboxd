9947e002a23710fbb26ab4c4b071b0862fae3c44
serena0322 <84858271+serena0322@users.noreply.github.com>
2025-10-28 01:17:11 +0100

    impostato livelli API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.musicboxd"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
